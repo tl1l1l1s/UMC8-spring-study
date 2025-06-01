f794746a00e208faff5655a53e01f5a2dbc56a50
tl1l1l1s <s2oo1og24@gmail.com>
2025-06-01 15:09:09 +0900

    ✨[Feat] OAUTH2를 사용한 구글 소셜 로그인 구현

--- src/main/java/umc/_th/spring/config/security/SecurityConfig.java
+++ src/main/java/umc/_th/spring/config/security/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import umc._th.spring.config.security.jwt.JwtAuthenticationFilter;
 import umc._th.spring.config.security.jwt.JwtTokenProvider;
@@ -29,12 +31,13 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/members/**",  "/auth/refresh", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/",  "/members/**",  "/auth/refresh", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/members/").permitAll()
                         .requestMatchers(HttpMethod.POST, "/members/login").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .oauth2Login(oauth2Login -> oauth2Login.successHandler(authenticationSuccessHandler()))
                 .csrf(csrf -> csrf.disable())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
@@ -44,5 +47,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new SimpleUrlAuthenticationSuccessHandler("/login");
     }
 }
